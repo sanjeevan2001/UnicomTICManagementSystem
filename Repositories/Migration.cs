a1fe6f485e57b1bef04144ac06d950d5e34df80c
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-06-11 13:00:56 +0530

    11.06.2025

---
+++ Repositories/Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicomTICManagementSystem.Repositories
+{
+    internal class Migration
+    {
+        public static void CreateTables() 
+        {
+            string tablesQuery = @"
+            CREATE TABLE IF NOT EXISTS Users(UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+Username TEXT NOT NULL, Password TEXT NOT NULL, Role TEXT NOT NULL);
+            CREATE TABLE IF NOT EXISTS Courses(CourseID INTEGER PRIMARY KEY AUTOINCREMENT,
+CourseName TEXT NOT NULL)/*;*/
+            /*create table IF NOT EXISTS Subject();*/
+
+
+
+            ";
+
+
+            using (var conn = DatabaseManager.GetConnection()) 
+            {
+                SQLiteCommand cmd = new SQLiteCommand(tablesQuery,conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
